ee85215dd319abf195703cec1cabcad4a73c5fc8
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2018-03-11 10:50:15 -0700

    deleted interfaces

--- app/src/main/kotlin/com/hmmelton/rescue/screens/splashscreen/ISplashscreenPresenter.kt
+++
@@ -1,9 +0,0 @@
-package com.hmmelton.rescue.screens.splashscreen
-
-/**
- * Created by harrisonmelton on 1/23/18.
- * Interface for splashscreen presenter
- */
-interface ISplashscreenPresenter {
-    fun onCreate()
-}
